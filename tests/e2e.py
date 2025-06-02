330f094cf12a33657cbe063776ac6f58f4bfe7a4
Thomas Chopitea <tomchop@gmail.com>
2025-06-02 18:38:50 +0000

    Fix test

--- tests/e2e.py
+++ tests/e2e.py
@@ -8,6 +8,10 @@ import requests
 from yeti import errors
 from yeti.api import YetiApi
 
+os.environ["YETI_ENDPOINT"] = "http://localhost:3000/"
+YETI_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWNwIiwic3ViIjoieWV0aSIsInNjb3BlcyI6WyJhbGwiXSwiY3JlYXRlZCI6IjIwMjUtMDYtMDJUMTY6NDI6NTQuNTgxOTg2WiIsImV4cCI6bnVsbCwibGFzdF91c2VkIjpudWxsLCJlbmFibGVkIjp0cnVlLCJleHBpcmVkIjpmYWxzZX0.sEWAL2yGfh2Vo_7HIiyvF2d7xWEskvsN4K6FuhJGNH8"
+os.environ["YETI_API_KEY"] = YETI_API_KEY
+
 
 class YetiEndToEndTest(unittest.TestCase):
     def setUp(self):
@@ -61,7 +65,7 @@ class YetiEndToEndTest(unittest.TestCase):
         )
         time.sleep(5)
         result = self.api.search_indicators(
-            name="testSear", description="tes", tags=["testTag"]
+            name="testSear", description="tes", tags=["testtag"]
         )
         self.assertEqual(len(result), 1, result)
         self.assertEqual(result[0]["name"], "testSearch")
